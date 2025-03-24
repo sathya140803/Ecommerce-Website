0edac2df0949c497970646ce50886205b27a3947
Pragnesh-0 <pragneshuno@gmail.com>
2025-03-24 15:01:43 +0400

    Update getgamesws.php
    small bug fix

--- PHPScripts/getgamesws.php
+++ PHPScripts/getgamesws.php
@@ -10,7 +10,7 @@ if(!isset($_GET["sortval"])||!isset($_SESSION["userid"])){
 
 require("../NuSoap/nusoap.php");
 
-$client = new nusoap_client("http://localhost/webasgt/WEB/server/server.php?wsdl");
+$client = new nusoap_client("http://localhost/WEB/server/server.php?wsdl");
 $result = $client->call("getGames",array(array("id"=>$_SESSION["userid"],"genre"=>$_GET["sortval"])));
 
 echo $client->getError();
